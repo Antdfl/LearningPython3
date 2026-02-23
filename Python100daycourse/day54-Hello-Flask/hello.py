87ab72a75abb056cacd6c48462495a6699a6d02c
Antonio DFL <antonio.difelice@tiscali.it>
2026-02-23 21:47:36 +0100

    other tests and practice with Flask

--- Python100daycourse/day54-Hello-Flask/hello.py
+++ Python100daycourse/day54-Hello-Flask/hello.py
@@ -4,12 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "<h1 style='text-align:center'>Hello, World!</h1>" \
+    "<p>This is a paragraph.</p>" \
+    "<img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDU3YnJjdzdvMXZ3YWpmMm1oaTlxeXM1a2ViN2VpeHRmNHU0MDQyeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kvrvnB158J4fm/giphy.gif'" \
+    " alt='Two kittens playfully interact with each other.' width='500'>"
+
 
 @app.route("/bye")
 def say_goodbye():
     return "Goodbye!"
 
+#@app.route("/username/<path:name>")
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there, {name.capitalize()}! You are {number} years old."
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
